802f812ee97cc4c589e896629291c0c7dc68ba35
Dayo Greats <dayogreats@gmail.com>
2023-02-28 03:33:23 -0500

    devise added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'static_page#landing_page'
   get  'static_page/policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
